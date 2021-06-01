ca7633b43907b7233895354aee10e91e2f28980f
satriowicaksono87 <satriowicaksono076@gmail.com>
2021-06-01 13:57:18 +0700

    first meet

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home extends CI_Controller
+{
+  public function index()
+  {
+    $this->load->view('main/index');
+  }
+  public function galeri()
+  {
+    $this->load->view('main/galeri');
+  }
+}
+
+?>
---
+++ application/views/main/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+    <title>Falasifa Indonesia</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="<?= base_url() ?>assets/style/css"> -->
+    <script src="https://use.fontawesome.com/633ebedb26.js"></script>
+    <style media="screen">
+    .linktree {
+  width: 200px;
+  height: 200px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+}
+.font-falasifa {
+  font-size: 14px;
+  background: -webkit-linear-gradient(#961D91, #F9026D);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.out-falasifa{
+  background: -webkit-linear-gradient(#961D91, #F9026D);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+    </style>
+</head>
+<body>
+    <div class="container">
+    <div class="col-xs-12">
+      <div class="text-center" style="padding-top: 40px; padding-bottom: 10px;">
+          <img class="backdrop linktree"style="background-image: url(<?= base_url() ?>assets/images/falasifa-big-logo.png);">
+          <h2 style="color: #ffffff; padding-top: 10px;">Falasifa Indonesia</h2>
+          <h5 class="">Daily and Party Dress</h5>
+          <hr style="height: 0,8px; color: black; background-color:#961D91;">
+      </div>
+
+    </div>
+    </div>
+
+    <img src="<?= base_url() ?>assets/images/falasifa-logo.png" alt="">
+    <div class="">
+    <div class="col-xs-12">
+            <div class="text-center">
+                <!-- <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2IZURI7'" type="button" class="btn btn-outline-primary shake text-dark" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 800;">15% OFF Instagram Growth</button>
+                </div> -->
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2SVZXES'" type="button" class="btn text-white" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600; background: -webkit-linear-gradient(#961D91, #F9026D); border: none;"> <span class="fa fa-whatsapp mr-2"></span> Whatsapp</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2SVZXES'" type="button" class="btn text-white" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600; background: -webkit-linear-gradient(#961D91, #F9026D); border: none;"> <span class="fa fa-instagram mr-2"></span> Instagram</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='<?= base_url() ?>home/galeri'; this.innerText='harap tunggu ..';" type="button" class="btn text-white" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600; background: -webkit-linear-gradient(#961D91, #F9026D); border: none;"> <span class="fa fa-mobile mr-2"></span> Gallery</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2SVZXES'" type="button" class="btn text-white" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600; background: -webkit-linear-gradient(#961D91, #F9026D); border: none;"> <span class="fa fa-usd mr-2"></span> Price List</button>
+                </div>
+                <!-- <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='http://bit.ly/2SVZXES'" type="button" class="btn btn-outline-info " style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Instagram</button>
+                </div> -->
+
+            </div>
+    </div>
+
+
+
+
+    </div>
+    <div class="container mb-3">
+      <div class="text-center">
+          <a href="" style="color: #34312f;"><b>Copyright &copy; Falasifa Indonesia</b></a>
+          <br><br>
+      </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- This pre-lander was created by Brandon Nilsson -->
+    <!-- You're welcome to edit, reproduce and change this template as long as original contributation stays present on the website at all times.  -->
+</body>
+
+</html>
